96264925c13cac95945544457ed05a94c0dd1612
Wobi Nurdiansyah <wobinurdiansyah20@gmail.com>
2024-12-17 09:22:35 +0700

    revisi form

--- domjs/minitaks1domjs/Day3domjs/index.js
+++ domjs/minitaks1domjs/Day3domjs/index.js
@@ -1,8 +1,36 @@
-document.getElementById('form').addEventListener('submit',function(event){
+const form = document.getElementById("form");
+form.addEventListener("submit", function (event) {
   event.preventDefault();
-  const getName1 = document.getElementById("namaDepan").value;
-  const getGender = document.getElementById("Gender").value;
- const formInput = FormData();
-  console.log(`nama anda ${getName1} dan gender anda ${getGender}`)
-  for
-})
+  const formData = new FormData(form);
+  const urlSearchParams = new URLSearchParams(formData).toString();
+  const saveData = document.getElementById("data");
+  const getNama = formData.get("name");
+  const getGender = formData.get("Gender");
+  saveData.innerHTML = `
+<p>Nama: ${getNama}</p>
+<p>Gender: ${getGender}</p>
+<pre>${urlSearchParams}</pre> 
+`;
+  {
+  }
+  console.log(getNama);
+  console.log(getGender);
+});
+
+
+const form2 = document.getElementById("myform2");
+form2.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData2 = new FormData(form2)
+  // data di simpan
+  const data = {}
+  formData2.forEach((value, key) =>{
+    data[key] = value;
+  });
+
+  const json = JSON.stringify(data)
+  const resultInput = document.getElementById("resultinput")
+  resultInput.innerHTML = `<span>${json}</span>`
+  console.log(json)
+});
+
